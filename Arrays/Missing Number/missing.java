1cb22e277f5485ae87f038084471aa60de3d0199
Gourab <gssarkargss2003@gmail.com>
2026-03-09 20:47:03 +0530

    Missing Number using XOR approach

---
+++ Arrays/Missing Number/missing.java
@@ -0,0 +1,25 @@
+public class missing
+{
+    public static void main (String [] args)
+    {
+        int arr[]=new int[3];
+        arr[0]=3;
+        arr[1]=0;
+        arr[2]=1;
+
+        System.out.print(missingElement(arr));
+    }
+    
+
+    public static int missingElement(int [] arr)
+    {
+        int XOR= arr.length;
+        for (int i = 0; i < arr.length; i++) {
+            
+            XOR= XOR ^ i;
+            XOR= XOR ^ arr[i];
+        }
+        return XOR;
+    }
+    
+}
