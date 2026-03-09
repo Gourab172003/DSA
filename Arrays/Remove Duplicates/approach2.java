2eb001af589e7c37c49f39a1f51cd9060020ee9a
Gourab <gssarkargss2003@gmail.com>
2026-03-09 17:52:31 +0530

    different approach

---
+++ Arrays/Remove Duplicates/approach2.java
@@ -0,0 +1,28 @@
+
+public class approach2{
+
+    public static int remove(int [] arr)
+    {
+      
+      int i=0;
+        for (int j=1;j<arr.length;j++)
+        {
+            if(arr[i]< arr[j])
+            {
+                int temp= arr[i+1];
+                arr[i+1]=arr[j];
+                arr[j]=temp;
+                 i++;
+            }
+           
+        }
+        return (i+1);
+    }
+
+    public static void main (String [] args)
+    {
+        int arr[]={0,1,1,1,2,2,3};
+        
+        System.out.print(remove(arr));
+    }
+}
