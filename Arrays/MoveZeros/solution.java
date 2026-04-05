fede22a273363593d625b7bbdf1581a11d0e801a
Gourab <gssarkargss2003@gmail.com>
2026-04-05 15:38:45 +0530

    Move zeros to the end

---
+++ Arrays/MoveZeros/solution.java
@@ -0,0 +1,20 @@
+public class solution{
+    public static void main (String [] args)
+    {
+        int a[]={0,1,0,3,12};
+        int k=0;
+        for(int i=1;i<a.length;i++){
+            if(a[i]!=0)
+            {
+                int temp=a[k];
+                a[k]=a[i];
+                a[i]=temp;
+                k++;
+            }
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]);
+        }
+    }
+}
