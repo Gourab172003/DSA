303906937d0563a204d455de7542a2870c0c4b69
Gourab <gssarkargss2003@gmail.com>
2026-04-14 13:11:02 +0530

    Bit Manipulation

---
+++ Bit Manipulation/swap.java
@@ -0,0 +1,15 @@
+public class swap
+{
+    public static void main (String args [])
+    {
+        int a=5;
+        int b=2;
+
+        a=a^b;
+        b=a^b;
+
+        a=a^b;
+        System.out.println(a);
+        System.out.print(b);
+    }
+}
