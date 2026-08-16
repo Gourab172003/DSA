afe5c086e8df440534a7b7bb9ba17cf0a7b0bceb
Gourab Sarkar <gssarkargss2003@gmail.com>
2026-08-16 23:51:51 +0530

    Time: 0 ms (100%), Space: 42.8 MB (83.96%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -8,7 +8,6 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
         int idx=1;
